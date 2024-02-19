b9fa3cccd5788ccd3d24f4789900a34c67e9e171
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-02-19 01:00:02 +0000

    Actually dont do that

--- src/RenderWebGL.js
+++ src/RenderWebGL.js
@@ -1975,13 +1975,6 @@ class RenderWebGL extends EventEmitter {
             y = Math.floor(drawable._position[1] + (sy - aabb.bottom));
         }
 
-        // USB: transform based on camera
-        x = this.translateX(
-            x, false, 1, true, y, 1
-        );
-        y = this.translateY(
-            y, false, 1, true, x, 1
-        );
         return [x, y];
     }
 
